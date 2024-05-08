94015b9e0932ef4644639803f502c9fd007458dc
W Cardoso <cardosu7@gmail.com>
2024-05-08 13:50:30 -0300

    adicionando classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Descrição do curso de Java");
+        curso.setCargaHoraria(8);
+
+        //System.out.println(curso);
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição da mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
 
 
     }
